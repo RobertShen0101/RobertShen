e302728c5550bf77f05206d93a15fd6e9702f59e
Haobo Shen <robert-shen@sjtu.edu.cn>
2025-01-30 05:32:26 +0800

    add enemy

--- script/main.js
+++ script/main.js
@@ -1,5 +1,24 @@
 const scope = document.getElementById('sniper-scope');
 
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+const enemyImagePath = 'src/enemy.png';
+
+const enemyImage = new Image();
+enemyImage.src = enemyImagePath;
+
+const numEnemies = 10; 
+
+
+function initenemy(){
+  for (let i = 0; i < numEnemies; i++) {
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    ctx.drawImage(enemyImage, x, y);
+  }
+}
+
+
 scope.style.backgroundImage = 'url("src/jingtou.png")';
 scope.style.backgroundSize = 'cover'; 
 scope.style.width = '100px';  
@@ -14,3 +33,5 @@ document.addEventListener('mousemove', (e) => {
   scope.style.left = `${mouseX - scopeSize / 2}px`;
   scope.style.top = `${mouseY - scopeSize / 2}px`;
 });
+
+
