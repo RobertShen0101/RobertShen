ad96baf8a4038fe34a93b41185b59be08463d60c
Haobo Shen <robert-shen@sjtu.edu.cn>
2025-01-31 04:52:56 +0800

    fix button

--- script/button.js
+++ script/button.js
@@ -1,3 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.getElementById('start-button').style.opacity = '1';
+    }, 3000); 
+});
+
+
 document.getElementById('start-button').addEventListener('click', function() {
     document.getElementById('game-cover').style.display = 'none';
     document.querySelector('.game-container').style.display = 'block';
